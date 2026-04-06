d3663f80a76c98ea2ba7422112b7a61ef190c89c
Yeshwanth-git-tech <yeshwanthsatheesh91@gmail.com>
2026-04-06 18:40:09 -0400

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        #LeetCode uses the original reference
+        #cant give new box , that is like just matrix = []
+        #here we are using the same referece matrix[:] - matrix.copy()
+        # matrix[:] = [list(row)for row in zip(*matrix[::-1])]
+
+        ##intiutive way
+
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i+1 , n):
+                matrix[i][j]  , matrix[j][i] = matrix[j][i] , matrix[i][j]
+
+        for row in matrix:
+            row.reverse()
+
+        
+        
