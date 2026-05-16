cef2445ddb0e54e0b34dfb423cd1c230670360af
Yeshwanth-git-tech <yeshwanthsatheesh91@gmail.com>
2026-05-15 23:47:30 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return Counter(s) == Counter(t)
+        hash1 = {}
+        hash2 = {}
+
+        for i in s:
+            hash1[i] = hash1.get(i , 0) + 1
+        for i in t:
+            hash2[i] = hash2.get(i , 0) + 1
+        
+        return hash1 == hash2
+        
